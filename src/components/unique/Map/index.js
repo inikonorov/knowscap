bb6d7669180827885ee009348f6a0cb1baa5fc96
Ivan Nikonorov <nikonorov1000@gmail.com>
2021-04-26 01:26:42 +0500

    added mechanism of centering map and highlighting country

--- src/components/unique/Map/index.js
+++ src/components/unique/Map/index.js
@@ -10,33 +10,83 @@ class Map extends React.Component {
 	constructor(props) {
 		super(props);
 
-		this.map = null;
+		this.mapContainer = null;
 	}
 
 	componentDidMount() {
 		const { center, zoom } = this.props;
 
-		const map = new mapboxgl.Map({
-			container: this.map,
-			style: 'mapbox://styles/mapbox/streets-v11',
+		this.map = new mapboxgl.Map({
+			container: this.mapContainer,
+			style: 'mapbox://styles/mapbox/light-v10',
 			center,
 			zoom,
+			logoPosition: 'bottom-right',
 		});
 
-		map.on('idle', this.props.setIsMapLoaded);
+		this.map.on('load', () => {
+			this.map.addLayer(
+				{
+					id: 'country-boundaries',
+					source: {
+						type: 'vector',
+						url: 'mapbox://mapbox.country-boundaries-v1',
+					},
+					'source-layer': 'country_boundaries',
+					type: 'fill',
+					paint: {
+						'fill-color': '#d2361e',
+						'fill-opacity': 0.4,
+					},
+				},
+				'country-label'
+			);
+
+			// TODO: хак используется для сброса дефолтного фильтра
+			this.map.setFilter('country-boundaries', ['in', 'iso_3166_1_alpha_3']);
+
+			this.map.addControl(
+				new mapboxgl.NavigationControl({
+					showCompass: false,
+				})
+			);
+
+			this.props.setIsMapLoaded();
+		});
+	}
+
+	componentDidUpdate(props) {
+		const { currentCountry } = this.props;
+
+		if (currentCountry.name !== props.currentCountry.name) {
+			this.map.flyTo({
+				center: currentCountry.latlng,
+				zoom: 6,
+			});
+
+			this.map.setFilter('country-boundaries', [
+				'in',
+				'iso_3166_1_alpha_3',
+				currentCountry.code,
+			]);
+		}
 	}
 
 	render() {
 		return (
 			<div>
-				<div ref={(el) => (this.map = el)} className="map" />
+				<div ref={(el) => (this.mapContainer = el)} className="map" />
 			</div>
 		);
 	}
 }
 
+const mapStateToProps = (state) => ({
+	currentCountry: state.currentCountry,
+});
+
 const mapDispatchToProps = (dispatch) => ({
 	setIsMapLoaded: () => dispatch(setIsMapLoaded()),
 });
 
-export default connect(null, mapDispatchToProps)(Map);
+export default connect(mapStateToProps, mapDispatchToProps)(Map);
--- src/helpers/mapCountry.js
+++ src/helpers/mapCountry.js
@@ -4,4 +4,5 @@ export default (country) => ({
 	name: escapeLatinExtendedSymbols(country.name),
 	capital: escapeLatinExtendedSymbols(country.capital),
 	code: country.alpha3Code,
+	latlng: [...country.latlng].reverse(),
 });
